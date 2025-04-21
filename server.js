c5979fbb6f03f23f724547da18671b28adcd5705
faaiz.hassan <faaiz.hassan@trangotech.com>
2025-04-21 13:09:24 +0500

    + exception handling

--- server.js
+++ server.js
@@ -57,6 +57,15 @@ app.use((err, req, res, next) => {
         next();
     }
 });
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection:', reason);
+    process.exit(1);
+});
 
 
 
